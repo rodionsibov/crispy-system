1e370349c285569b70cc8817c1836df22c508649
Rodion Sibov <rodionsibov@gmail.com>
2021-12-10 10:29:11 +0200

    update

--- src/store/index.js
+++ src/store/index.js
@@ -57,9 +57,9 @@ export default createStore({
     addProductToCart(context, product) {
       if (product.inventory > 0) {
         const cartItem = context.state.cart.find(item => item.id === product.id)
-
+        
         if (!cartItem) {
-          context.commit('pushProductToCart', product)
+          context.commit('pushProductToCart', product.id)
         } else {
           context.commit('incrementItemQuantity', cartItem)
         }
